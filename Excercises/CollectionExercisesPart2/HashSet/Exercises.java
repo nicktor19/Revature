f960ea72c605846780e6d1b1c3d04ced1dfa1bba
Nicholas Torres <nicholastorres@gmx.com>
2022-04-26 13:39:06 -0400

    Finished all of HashSets

--- Excercises/CollectionExercisesPart2/HashSet/Exercises.java
+++ Excercises/CollectionExercisesPart2/HashSet/Exercises.java
@@ -10,11 +10,27 @@ public class Exercises {
 
     public static void main(String[] args) {
         HashSet<String> names = UserInput.createHashSet(); // store a Hashset once for rest of programs.
-
-        new Exercise1(names); //1. Write a Java program to append the specified element to the end of a hash set.
-//        2. Write a Java program to iterate through all elements in a hash list.
-//        3. Write a Java program to get the number of elements in a hash set.
-//        4. Write a Java program to empty an hashset.
+        System.out.println("Select a Program to run: 1-4");
+        System.out.println("1. Write a Java program to append the specified element to the end of a hash set.\n" +
+                "2. Write a Java program to iterate through all elements in a hash list.\n" +
+                "3. Write a Java program to get the number of elements in a hash set.\n" +
+                "4. Write a Java program to empty an hash set.");
+        switch (UserInput.inputInt()) {
+            case 1:
+                new Exercise1(names); //1. Write a Java program to append the specified element to the end of a hash set.
+                break;
+            case 2:
+                new Exercise2(names); //2. Write a Java program to iterate through all elements in a hash list.
+                break;
+            case 3:
+                new Exercise3(names);// 3. Write a Java program to get the number of elements in a hash set.
+                break;
+            case 4:
+                new Exercise4(names);// 4. Write a Java program to empty a hashset.
+                break;
+            default:
+                break;
+        }
     }
 }
 //all classes will need a constructor that takes a LinkedList
@@ -49,11 +65,44 @@ class Exercise1 {
 class Exercise2{
     //constructor
     public Exercise2(HashSet<String> x) {
-
+        int i = 0;
+        Iterator it = x.iterator();
+        System.out.println("\n2. Write a Java program to iterate through all elements in a hash list.");
+        System.out.println("Before: " + x);
+        while (it.hasNext()) {
+            System.out.println(++i + ". " + it.next());
+        }
     }
 
     //methods
 
+}
+
+//3. Write a Java program to get the number of elements in a hash set.
+class Exercise3{
+    //constructor
+    public Exercise3(HashSet<String> x) {
+        int i = 0;
+        System.out.println("3. Write a Java program to get the number of elements in a hash set.");
+        UserInput.printHashSetNumbered(x);
+        Iterator it = x.iterator();
+        while (it.hasNext()) {
+            it.next();
+            i++;
+        }
+        System.out.println("You have \"" + i + "\" elements in your list.");
+    }
+}
+
+// 4. Write a Java program to empty a hashset.
+class Exercise4{
+    //constructor
+    public Exercise4(HashSet<String> x) {
+        System.out.println("// 4. Write a Java program to empty a hashset.");
+        UserInput.printHashSetNumbered(x); // print out x before
+        x.clear();
+        System.out.println("after: " + x);
+    }
 }
 
 //user input class.
